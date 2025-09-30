227fbd2d53d56cd106bebbf3de176242cb19cb51
Sergio Sacristan <sergiosacristan8@gmail.com>
2025-09-30 20:11:03 +0200

    infinity scroll

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,7 +1,10 @@
 import {
   Component,
+  ElementRef,
   inject,
   OnInit,
+  PLATFORM_ID,
+  ViewChild,
 } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,6 +12,7 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogContentDialog } from './dialog/dialog';
 import { PokemonService } from '../../services/pokemon.service';
 import { PokemonStore } from '../../services/pokemon.store';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-card',
   imports: [MatCardModule, MatDialogModule, MatButtonModule],
@@ -17,7 +21,7 @@ import { PokemonStore } from '../../services/pokemon.store';
 })
 export class Card implements OnInit {
   readonly dialog = inject(MatDialog);
-
+  private platformId = inject(PLATFORM_ID);
   openDialog() {
     const dialogRef = this.dialog.open(DialogContentDialog);
 
@@ -26,14 +30,32 @@ export class Card implements OnInit {
     });
   }
 
+
+  @ViewChild('anchor', { static: true }) anchor!: ElementRef<HTMLDivElement>;
+  ngAfterViewInit(): void {
+
+    if (!isPlatformBrowser(this.platformId)) return;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          this.loadMore();
+        }
+      },
+      { threshold: 0.5 } // cuando se vea el 50% del div
+    );
+
+    observer.observe(this.anchor.nativeElement);
+  }
+
   protected pokemonService = inject(PokemonService);
   protected pokemonStore = inject(PokemonStore);
   protected pokemons = this.pokemonStore.pokemons;
 
   ngOnInit(): void {
-    this.pokemonStore.addPokemons(); 
+    this.pokemonStore.addPokemons();
   }
   loadMore(): void {
     this.pokemonStore.addPokemons();
   }
 }
+
